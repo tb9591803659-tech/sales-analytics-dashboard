2f75c89f27fd6a9245240a2e3648596b8e149cf7
Shashank T <tb9591803659@gmail.com>
2026-08-26 16:34:54 +0530

    Completed day 2 ,Added sales analystics engine and business analysis

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from src.data_loader import load_data, save_data
 from src.cleaner import clean_data,validate_cleaned_data
-from src.analysis import total_sales
+from src.analysis import total_sales,total_orders,total_profit,total_quantity,average_order_value,profit_margin,sales_by_category,profit_by_category,profit_by_region,sales_by_region,quantity_by_category,category_summary,regional_summary,yearly_sales,monthly_profit,monthly_sales,top_products_by_sales,top_customers_by_sales,product_performance,top_products_by_profit,correlation_analysis
 import os
-
 
 filepath = input("Enter file name: ")
 
@@ -12,7 +11,7 @@ if not filepath.endswith(".csv"):
 filepath = os.path.join("data", filepath)
 
 df = load_data(filepath)
-
+1
 print(df.columns.tolist())
 if df is not None:
 
@@ -22,7 +21,53 @@ if df is not None:
 
     save_data(df, output_path)
 
-    validate_cleaned_data(df)
+    # validate_cleaned_data(df)
 
     Total_Sales = total_sales(df)
     print("Total  Sales :",Total_Sales)
+    print("Total profit :",total_profit(df))
+    print("Total Orders :",total_orders(df))
+    print("Total Quantity :",total_quantity(df))
+    print("Average order Value(AOV) :",average_order_value(df))
+    print("Profit Margin: ",profit_margin(df))
+    
+    # print("\nSales by Category:")
+    # print(sales_by_category(df))
+
+    # print("\nSales by Region:")
+    # print(sales_by_region(df))
+
+    # print("\nProfit by Category:")
+    # print(profit_by_category(df))
+
+    # print("\nProfit by Region:")
+    # print(profit_by_region(df))
+
+    # print("\nQuantity by Category:")
+    # print(quantity_by_category(df))
+    # print("\nCategorical Summary: \n",category_summary(df))
+    # print("\nRegional Summary: \n",regional_summary(df)) 
+    # print("\nYearly Sales:")
+    # print(yearly_sales(df))
+
+    # print("\nMonthly Sales:")
+    # print(monthly_sales(df))
+
+    # print("\nMonthly Profit:")
+    # print(monthly_profit(df))
+
+    # print("\nTop Products by Sales:")
+    # print(top_products_by_sales(df))
+
+    # print("\nTop Products by Profit:")
+    # print(top_products_by_profit(df))
+
+    # print("\nTop Customers by Sales:")
+    # print(top_customers_by_sales(df))
+
+    # print("\nProduct Performance:")
+    # print(product_performance(df).head(10))
+
+    print("\nCorrelation Analysis:")
+    print(correlation_analysis(df))
+
--- src/analysis.py
+++ src/analysis.py
@@ -1,12 +1,157 @@
+import pandas as pd 
 
 def total_sales(df):
-    return df["Sales"].sum()
+    return round(df["Sales"].sum(),2)
 
 def total_profit(df):
-    return df["Profit"].sum()
+    return round(df["Profit"].sum(),2)
 
 def total_orders(df) :
-    return df["Order ID"].nunique()
+    return df["Order.ID"].nunique()
 
 def total_quantity(df):
     return df["Quantity"].sum()
+
+def average_order_value(df):
+    total_sales = df["Sales"].sum()
+    total_orders = df["Order.ID"].nunique()
+
+    return round(total_sales / total_orders,2)
+
+def profit_margin(df) :
+    total_sales = df["Sales"].sum()
+    total_profit = df["Profit"].sum()
+    if total_sales == 0 :
+        return 0
+    return round((float)(total_profit/total_sales)*100,2)
+
+def sales_by_category(df):
+    return df.groupby("Category")["Sales"].sum().reset_index()
+
+def sales_by_region(df):
+    return df.groupby("Region")["Sales"].sum().reset_index()
+
+def profit_by_category(df):
+    return df.groupby("Category")["Profit"].sum().reset_index()
+
+def profit_by_region(df):
+    return df.groupby("Region")["Profit"].sum().reset_index()
+
+def quantity_by_category(df):
+    return df.groupby("Category")["Quantity"].sum().reset_index()
+
+def category_summary(df):
+    return df.groupby("Category").agg({
+    "Sales": ["sum", "mean", "max", "min"],
+    "Profit": ["sum", "mean"],
+    "Quantity": "sum"
+})
+
+def regional_summary(df):
+    return df.groupby("Region").agg({
+        "Sales": ["sum", "mean", "max", "min"],
+        "Profit": "sum",
+        "Quantity": "sum"
+    }).reset_index()
+
+def yearly_sales(df):
+    analysis_df = df.copy()
+
+    analysis_df["Year"] = analysis_df["Order.Date"].dt.year
+
+    return (
+        analysis_df
+        .groupby("Year")["Sales"]
+        .sum()
+        .reset_index()
+    )
+
+def monthly_sales(df):
+    analysis_df = df.copy()
+
+    analysis_df["Year"] = analysis_df["Order.Date"].dt.year
+    analysis_df["Month"] = analysis_df["Order.Date"].dt.month
+
+    return (
+        analysis_df
+        .groupby(["Year", "Month"])["Sales"]
+        .sum()
+        .reset_index()
+    )
+
+def monthly_profit(df):
+    analysis_df = df.copy()
+
+    analysis_df["Year"] = analysis_df["Order.Date"].dt.year
+    analysis_df["Month"] = analysis_df["Order.Date"].dt.month
+
+    return (
+        analysis_df
+        .groupby(["Year", "Month"])["Profit"]
+        .sum()
+        .reset_index()
+    )
+
+def top_products_by_sales(df, n=10):
+    result = (
+        df.groupby("Product.Name")["Sales"]
+        .sum()
+        .reset_index()
+    )
+
+    return result.sort_values(
+        "Sales",
+        ascending=False
+    ).head(n)
+
+def top_products_by_profit(df, n=10):
+    result = (
+        df.groupby("Product.Name")["Profit"]
+        .sum()
+        .reset_index()
+    )
+
+    return result.sort_values(
+        "Profit",
+        ascending=False
+    ).head(n)
+
+def top_customers_by_sales(df, n=10):
+    result = (
+        df.groupby(["Customer.ID", "Customer.Name"])["Sales"]
+        .sum()
+        .reset_index()
+    )
+
+    return result.sort_values(
+        "Sales",
+        ascending=False
+    ).head(n)
+
+def product_performance(df):
+    result = (
+        df.groupby("Product.Name")
+        .agg({
+            "Sales": "sum",
+            "Profit": "sum",
+            "Quantity": "sum"
+        })
+        .reset_index()
+    )
+
+    result["Profit Margin"] = (
+    result["Profit"]
+    .div(result["Sales"].replace(0, pd.NA))
+    .mul(100)
+    )
+    return result
+
+def correlation_analysis(df):
+    columns = [
+        "Sales",
+        "Profit",
+        "Quantity",
+        "Discount"
+    ]
+
+    return df[columns].corr()
